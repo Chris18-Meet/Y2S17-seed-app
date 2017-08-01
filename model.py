078c5a1b6a5f2792e054fbdaaf623360d8dcc449
hadar18-meet <hadarky@gmail.com>
2017-08-01 15:29:19 +0300

    hadar

--- model.py
+++ model.py
@@ -3,10 +3,34 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class YourModel(Base):
-    __tablename__  = 'yourmodel'
+class Ideas(Base):
+    __tablename__  = 'Ideas'
     id             = Column(Integer, primary_key=True)
+    name           = Column(String)
+    describtion    = Column(String)
+    looking_for    = Column(String)
+    owner          = Column(String)
+    likes          = Column(Integer)
+    category       = Column(String)
+
     # ADD YOUR FIELD BELOW ID
 
-# IF YOU NEED TO CREATE OTHER TABLE 
+class Comment(Base):
+    __tablename__  = 'Comment'
+    id             = Column(Integer, primary_key=True)
+    comment        = Column(String)
+    idea           = Column(String)
+    owner          = Column(String)
+
+class Users(Base):
+    __tablename__  = 'Users'
+    id             = Column(Integer, primary_key=True)
+    first_name     = Column(String)
+    last_name      = Column(String)
+    email          = Column(String)
+    password       = Column(String)
+    profession     = Column(String)
+    linkedin_url   = Column(String)
+
+    # IF YOU NEED TO CREATE OTHER TABLE 
 # FOLLOW THE SAME STRUCTURE AS YourModel
