53052d88e818c83e5ae98fd8d3ef54fb57715aac
Malak18-Meet <malak.jabarin@gmail.com>
2017-08-01 17:47:49 +0300

    all

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
+from flask_login import UserMixin
+
 from sqlalchemy import Column, DateTime, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
+from werkzeug.security import generate_password_hash, check_password_hash
 Base = declarative_base()
 
 class Idea(Base):
@@ -22,16 +25,24 @@ class Comment(Base):
     idea           = Column(String)
     owner          = Column(String)
 
-class User(Base):
+class User(UserMixin, Base):
     __tablename__  = 'Users'
     id             = Column(Integer, primary_key=True)
     first_name     = Column(String)
     last_name      = Column(String)
     email          = Column(String)
-    password       = Column(String)
+    pw_hash        = Column(String)
     profession     = Column(String)
     linkedin_url   = Column(String)
     photo          = Column(String)
+    authenticated  =Column(Boolean,default=False)
+    
+    def __repr__(self):
+      return "<User: %s, password: %s>" % (
+        self.email, self.pw_hash)
 
-    # IF YOU NEED TO CREATE OTHER TABLE 
-# FOLLOW THE SAME STRUCTURE AS YourModel
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
